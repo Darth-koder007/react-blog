652873083503d2ca7b31961e4d3b385c36e06e8b
Vijay Singh <vj2010sn@gmail.com>
2017-09-11 15:31:00 +0530

    validations added in new posts component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
-
+import { Field, reduxForm } from 'redux-form';
 class PostsNew extends Component {
-    render() {
-        return (
-            <div>
-                PostsNew!
-            </div>
-        );
-    }
+	renderField(field) {
+		return (
+			<div>
+				<label>{field.label}</label>
+				<input
+					type="text"
+					{...field.input}
+				/>
+				{field.meta.error}
+			</div>
+		);
+	}
+
+	render() {
+		return (
+			<form>
+				<Field
+					label="Title"
+					name="title"
+					component={this.renderField}
+				/>
+				<Field
+					label="Categories"
+					name="categories"
+					component={this.renderField}
+				/>
+				<Field
+					label="Post Content"
+					name="content"
+					component={this.renderField}
+				/>
+			</form>
+		);
+	}
 }
 
-export default PostsNew;
+function validate(values) {
+	const errors = {};
+
+	// Validate the inputs from 'values'
+	if (!values.title) {
+		errors.title = "Enter a title"; 
+	}
+
+	if (!values.categories) {
+		errors.categories = "Enter some categories"; 
+	}
+
+	if (!values.content) {
+		errors.content = "Enter some content"; 
+	}
+
+	// If errors is empty, its good to go
+	// else form invalid assumed by redux-form
+	return errors;
+}
+
+export default reduxForm({
+	validate,
+	form: 'PostsNewForm'
+})(PostsNew);
